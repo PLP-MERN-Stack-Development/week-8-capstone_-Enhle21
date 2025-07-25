e43e03077240e4b36a7ca26725662987f7fb7270
unknown <tshabangupalesa272@gmail.com>
2025-07-25 17:32:47 +0200

    Fix duplicate mongoose declaration

--- server/server.js
+++ server/server.js
@@ -54,9 +54,6 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// server.js 
-const mongoose = require('mongoose');
-
 // Replace with your real Mongo URI
 mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://Enhleza:UaelyVHlNmSA1T4Z@cluster0.pdx7fx4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
